d9996426a1d9dcf11af5b82002bd3d1e3de8ae47
hirotanniraisei <sdt2495gp0013@edu.sca.ac.jp>
2024-11-28 12:18:29 +0900

    追加

---
+++ 2班/project/Source/Player.cpp
@@ -0,0 +1,43 @@
+#include "Player.h"
+#include <DxLib.h>
+#include <cassert>
+
+Player::Player()
+{
+	hImage = LoadGraph("ƒQ[ƒ€‘fÞ/rocket_01.png");
+	assert(hImage > 0);
+
+	position.x = 0;
+	position.y = 0;
+}
+
+Player::~Player()
+{
+	DeleteGraph(hImage);
+}
+
+void Player::Update()
+{
+	if (hImage) {
+		position.x += 2.0f;
+	}
+	else {
+		if (CheckHitKey(KEY_INPUT_D) != 0)
+			position.x += 3.0f;
+		if (CheckHitKey(KEY_INPUT_A) != 0)
+			position.x -= 3.0f;
+		if (CheckHitKey(KEY_INPUT_W) != 0)
+			position.y -= 5.0f;
+		if (CheckHitKey(KEY_INPUT_S) != 0)
+			position.y += 5.0f;
+		if (CheckHitKey(KEY_INPUT_Q) != 0)
+			position.y += 1.0f;
+		if (CheckHitKey(KEY_INPUT_E) != 0)
+			position.y -= 1.0f;
+	}
+}
+
+void Player::Draw()
+{
+	DrawGraph(position.x, position.y, hImage, TRUE);
+}
--- 2班/project/Source/Player.h
+++ 2班/project/Source/Player.h
@@ -1,1 +1,17 @@
 #pragma once
+#include "../Library/GameObject.h"
+#include "Vector2.h"
+class Player : public GameObject
+{
+public:
+	Player();
+	~Player();
+	void Update();
+	void Draw();
+	VECTOR2 position;
+
+	int hImage;
+
+	bool prevDushKey;
+
+};
