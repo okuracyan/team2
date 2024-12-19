b433def6ed1021c1c9183223a165c3f64bfe1f37
okuracyan <sdt2495sc0001@edu.sca.ac.jp>
2024-12-19 11:37:18 +0900

    スコア途中

---
+++ 2班/project/Source/Score.cpp
@@ -0,0 +1,13 @@
+#include "Score.h"
+
+Score::Score()
+{
+}
+
+Score::~Score()
+{
+}
+
+void Score::DispScore(int x, int y)
+{
+}
--- 2班/project/Source/Score.h
+++ 2班/project/Source/Score.h
@@ -1,2 +1,19 @@
 #pragma once
-#include "Player.h"
+#include <Dxlib.h>
+class Score
+{
+private:
+	const char* NUMBER_GRAPH_PATH = "data/Graph/Number/number_%d.png";
+	// 数値画像のファイル名
+	static const int NUMBER_WIDTH = 32; // 数値の幅
+	static const int MAX_DIGITS = 10; // 最大桁数()
+private:
+	int score;
+	int numberHandle[10]; // 数値画像ハンドル
+public:
+	Score();
+	~Score();
+	void DispScore(int x, int y); // スコアの表示
+	void SetScore(int score) { this->score = score; }// スコアの登録
+	void AddScore(int point) { score += point; } // スコアの加算
+};
